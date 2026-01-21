015716c9b9308120f6f7477be52a3e80d68c81b2
Native315 <45272908+native315@users.noreply.github.com>
2026-01-20 21:51:22 -0500

    initial commit (sorry for not doing this by the deadline)

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // JavaScript
+const mutebutton = document.getElementById("mutebutton");
+const music = document.getElementById("music");
+
+mutebutton.addEventListener("click", (event)=> {
+    music.muted = !music.muted;
+})
+
