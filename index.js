cec7a05f407a286e06e0090d448becc38db3d58e
Native315 <45272908+native315@users.noreply.github.com>
2026-01-20 23:13:45 -0500

    more fixes + volume slider lol

--- index.js
+++ index.js
@@ -2,7 +2,25 @@
 const mutebutton = document.getElementById("mutebutton");
 const music = document.getElementById("music");
 
+const slider = document.getElementById("volumeslider")
+const label = document.getElementById("demo")
+
 mutebutton.addEventListener("click", (event)=> {
+    if (music.muted==true) {
+        mutebutton.textContent = "Mute Music"
+    } else {
+        mutebutton.textContent = "Unmute Music"
+    }
     music.muted = !music.muted;
 })
 
+label.innerHTML = slider.value;
+
+slider.oninput = function() {
+    label.innerHTML = this.value;
+    music.volume = this.value/100;
+}
+
+document.addEventListener("click", () => {
+    music.play();
+}, { once: true });
